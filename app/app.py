f10f6890a10972a0334749fe800b8ae248948bf4
UlisesME <ulises.st.99@gmail.com>
2023-04-28 18:48:51 -0500

    app.py updated

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, everyone!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
